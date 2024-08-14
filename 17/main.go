895aabee6c19b6ef9ef34967afa3dd54ff0cd1f2
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-08-14 13:14:11 -0300

    Empty interfaces

---
+++ 17/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type x interface{}
+
+func main() {
+	var x interface{} = 10
+	var y interface{} = "Hello, World"
+
+	showType(x)
+	showType(y)
+
+}
+
+func showType(t interface{}) {
+	fmt.Printf("O tipo da variável é %T e o valor é %v\n", t, t)
+}
