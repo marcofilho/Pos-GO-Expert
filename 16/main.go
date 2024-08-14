455cfaf88194ebec7a731f65ff9ec88c1d9f2f7e
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-08-14 12:57:15 -0300

    Ponteiros

--- 16/main.go
+++ 16/main.go
@@ -1,5 +1,39 @@
 package main
 
+import "fmt"
+
+type Cliente struct {
+	nome string
+}
+
+type Conta struct {
+	saldo int
+}
+
+func (c *Conta) simular(valor int) int {
+	c.saldo += valor
+	println(c.saldo)
+	return c.saldo
+}
+
+func (c *Cliente) andou() {
+	c.nome = "Marco Lima"
+	fmt.Printf("O cliente %v andou\n", c.nome)
+}
+
 func main() {
 
+	marco := Cliente{
+		nome: "Marco",
+	}
+
+	marco.andou()
+	fmt.Printf("O nome do cliente é %v\n", marco.nome)
+
+	conta := Conta{
+		saldo: 100,
+	}
+
+	conta.simular(100)
+	println(conta.saldo)
 }
