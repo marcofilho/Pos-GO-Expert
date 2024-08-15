32a4f8be931517d57dcc66776d2a5f26e4cd5e1b
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-08-15 01:28:53 -0300

    Packages and modules

--- 20/main.go
+++ 20/main.go
@@ -7,4 +7,12 @@ import (
 
 func main() {
 	fmt.Println("O resultado da soma é:", matematica.Soma(10, 20))
+
+	carro := matematica.Carro{Marca: "Honda"}
+	fmt.Println(carro.Marca)
+
+	var b int = matematica.A + 10
+	fmt.Println(b)
+
+	fmt.Println(carro.Andar())
 }
--- 20/matematica/matematica.go
+++ 20/matematica/matematica.go
@@ -3,3 +3,13 @@ package matematica
 func Soma[T int | float64](a T, b T) T {
 	return a + b
 }
+
+var A int = 10
+
+type Carro struct {
+	Marca string
+}
+
+func (c Carro) Andar() string {
+	return "O carro está andando"
+}
