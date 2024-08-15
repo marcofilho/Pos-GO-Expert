437390be53985fc824458d54e3cdaa654915758f
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-08-15 00:40:34 -0300

    Type Assertion

---
+++ 18/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var minhaVar interface{} = "Marco Lima"
+	println(minhaVar)
+	println(minhaVar.(string))
+
+	res, ok := minhaVar.(int)
+	fmt.Printf("O valor de res é %v e o resultado de ok é %v\n", res, ok)
+
+	res2 := minhaVar.(int)
+	fmt.Printf("O valor de res2 é %v", res2)
+
+}
