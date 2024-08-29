98e765b50d524dc650e3ac40fcb639abc3a5c631
Marco Antonio Barcellos Lima Filho <m.barceloslima@gmail.com>
2024-08-29 11:34:34 -0300

    Templates

---
+++ Important Packages/08/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+func main() {
+	curso := Curso{"Golang", 40}
+
+	tmp := template.New("CursoTemplate")
+
+	tmp, _ = tmp.Parse("O curso {{.Nome}} tem carga horária de {{.CargaHoraria}} horas")
+
+	err := tmp.Execute(os.Stdout, curso)
+
+	if err != nil {
+		panic(err)
+	}
+}
