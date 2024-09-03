28d6dc81b78354c77f8f4b84f193ded527b87c11
Marco Filho <m.barceloslima@gmail.com>
2024-09-03 16:28:52 -0300

    Http Timeout

---
+++ Important Packages/09/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	client := http.Client{
+		Timeout: time.Duration(time.Microsecond),
+	}
+
+	resp, err := client.Get("https://google.com")
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Status Code:", resp.StatusCode)
+	fmt.Println("Body:", string(body))
+}
