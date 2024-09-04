f8c5157608d0a3db409a10d59dcf8b5101f5420a
Marco Filho <m.barceloslima@gmail.com>
2024-09-04 17:13:31 -0300

    GORM soft delete

--- Database/02/main.go
+++ Database/02/main.go
@@ -11,10 +11,11 @@ type Product struct {
 	ID    int     `gorm:"primaryKey" json:"id"`
 	Name  string  `json:"name"`
 	Price float64 `json:"price"`
+	gorm.Model
 }
 
 func main() {
-	dsn := "root:root@tcp(localhost:3306)/goexpert"
+	dsn := "root:root@tcp(localhost:3306)/goexpert?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -23,11 +24,12 @@ func main() {
 	db.AutoMigrate(&Product{})
 
 	//create single record
-	db.Create(&Product{
-		Name:  "Laptop",
-		Price: 1000})
+	// db.Create(&Product{
+	// 	Name:  "Laptop",
+	// 	Price: 1000})
 
 	products := []Product{
+		{Name: "Laptop", Price: 1500},
 		{Name: "Mouse", Price: 10},
 		{Name: "Keyboard", Price: 20},
 		{Name: "Monitor", Price: 200},
@@ -37,9 +39,35 @@ func main() {
 	db.Create(&products)
 
 	//read all records
-	db.Find(&products)
+	//db.Find(&products)
 
-	for _, product := range products {
-		fmt.Printf("Product: %s, Price: %.2f\n", product.Name, product.Price)
-	}
+	// for _, product := range products {
+	// 	fmt.Printf("Product: %s, Price: %.2f\n", product.Name, product.Price)
+	// }
+
+	//find records with where conditions
+
+	//var products []Product
+	// db.Where("price > ?", 50).Find(&products)
+	// for _, product := range products {
+	// 	fmt.Printf("Product: %s, Price: %.2f\n", product.Name, product.Price)
+	// }
+
+	//find strings records with like condition
+
+	// db.Where("name LIKE ?", "%k%").Find(&products)
+	// for _, product := range products {
+	// 	fmt.Printf("Product: %s, Price: %.2f\n", product.Name, product.Price)
+	// }
+
+	// var product Product
+	// db.First(&product, 1)
+	// product.Name = "New Laptop"
+	// db.Save(&product)
+
+	var product2 Product
+	db.First(&product2, 1)
+	fmt.Println(product2.Name, product2.Price)
+
+	db.Delete(&product2)
 }
