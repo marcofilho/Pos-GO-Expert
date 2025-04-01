decf0f706905b68882e01a6ec6a783c27fa2a2f4
Marco Antonio Filho <m.barceloslima@gmail.com>
2025-03-31 22:35:14 -0700

    Refactor go.mod and go.sum to remove unused sqlite3 dependency; comment out main function for future implementation

--- Dependency Injection/main.go
+++ Dependency Injection/main.go
@@ -1,28 +1,33 @@
+// package main
+
+// import (
+// 	"database/sql"
+// 	"fmt"
+
+// 	"github.com/marcofilho/Pos-GO-Expert/DependencyInjection/product"
+// 	_ "github.com/mattn/go-sqlite3"
+// )
+
+// func main() {
+// 	db, err := sql.Open("sqlite3", "./test.db")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	repo := product.NewProductRepository(db)
+
+// 	usecase := product.NewProductUsecase(repo)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	product, err := usecase.GetProduct(1)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fmt.Printf("Product ID: %d, Product Name: %s", product.ID, product.Name)
+
+// }
+
 package main
-
-import (
-	"database/sql"
-	"fmt"
-
-	"github.com/marcofilho/Pos-GO-Expert/DependencyInjection/product"
-	_ "github.com/mattn/go-sqlite3"
-)
-
-func main() {
-	db, err := sql.Open("sqlite3", "./test.db")
-	if err != nil {
-		panic(err)
-	}
-
-	repo := product.NewProductRepository(db)
-
-	usecase := product.NewProductUsecase(repo)
-
-	product, err := usecase.GetProduct(1)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Product ID: %d, Product Name: %s", product.ID, product.Name)
-
-}
